736c0b3972083e67faab624107930e41d0ed04da
Alan Pich <alan.pich@gmail.com>
2013-05-14 09:26:18 +0100

    Dead as a doornail

--- core/components/ImagePlus/imageplus.conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	define("ImagePlusCore",$modx->getOption('core_path').'components/ImagePlus/');
-	define("ImagePlusAssets",$modx->getOption('manager_url').'assets/components/ImagePlus/');
-
-
-?>
